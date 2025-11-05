c9883bca64d6760c2e8fca36b2fc620d9de7ceb5
CAMARA Diangou <dc253376@c60h4xkvnf.edu.atlas.local>
2025-11-05 08:41:23 +0100

    First commit

---
+++ whatToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatToWatchApp.swift
+//  whatToWatch
+//
+//  Created by CAMARA Diangou on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct whatToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
